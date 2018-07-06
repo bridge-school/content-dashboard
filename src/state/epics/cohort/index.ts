5c0034d4d14856e1f60f76ad6f1d699b9938c971
Igor Barsi <ibarsi92@gmail.com>
2018-07-06 13:02:49 -0400

    ✨ Added start date input to cohort home screen that captures and stores the start date in the store, as well as providing it to firebase when creating a cohort.

--- src/state/epics/cohort/index.ts
+++ src/state/epics/cohort/index.ts
@@ -4,7 +4,11 @@ import { setCohort, allCohortsUpdated$ } from '../../../firebaseconfig';
 
 export const addCohortEpic = $action =>
   $action.ofType(TypeKeys.CREATE_COHORT).pipe(
-    map(({payload}) => payload),
+    map(({ payload }) => ({
+      ...payload,
+      startDate: (new Date(payload.startDate)).toISOString(),
+      endDate: (new Date(payload.endDate)).toISOString()
+    })),
     mergeMap(({ cohortName, moduleIds, startDate, endDate }) => setCohort(cohortName, moduleIds, startDate, endDate)),
     map((cohort: any) => ({
       type: TypeKeys.ROUTE_TO,
@@ -17,9 +21,9 @@ export const setCohortDataByRouteEpic = ($action) =>
     .pipe(
       filter((action: any) => action.payload.pathname.includes('/cohort/')),
       map((action: any) => ({ type: TypeKeys.SET_SELECTED_COHORT, payload: action.payload.pathname.split('/')[2] })),
-    );
+  );
 
 export const setAllCohorts = () =>
   allCohortsUpdated$.pipe(
-    map(cohorts => ({type: TypeKeys.SET_ALL_COHORTS, payload: cohorts}))
+    map(cohorts => ({ type: TypeKeys.SET_ALL_COHORTS, payload: cohorts }))
   );

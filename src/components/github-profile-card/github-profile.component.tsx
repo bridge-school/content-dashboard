efd16fdaf94bcece6cf0940c58ef688a8bc0c73f
Abdella Ali <della@Abdellas-MacBook-Pro.local>
2018-08-09 15:32:26 -0400

    simple auth guard against creating a lesson, auth badge, and other odds and ends

---
+++ src/components/github-profile-card/github-profile.component.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { WithStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+const styles = (theme) => ({
+  card: {
+    display: 'flex',
+    width: 200,
+    height: 60
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column' as any,
+    padding: 7
+  },
+  content: {
+    flex: '2 0 auto',
+    padding: '0'
+  },
+  cover: {
+    flexGrow: 1
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {
+  user: any;
+}
+
+export const GithubProfileCard = withStyles(styles)
+((props: Props) => (
+  <Card className={props.classes.card}>
+    <div className={props.classes.details}>
+      <CardContent className={props.classes.content}>
+        <Typography variant="headline">{props.user.displayName}</Typography>
+        <Typography variant="subheading" color="textSecondary">
+          {props.user.email}
+        </Typography>
+      </CardContent>
+    </div>
+    <CardMedia
+      className={props.classes.cover}
+      image={props.user.photoURL}
+      title={props.user.displayName}
+    />
+  </Card>
+));
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -3,11 +3,13 @@ import { combineReducers, Reducer } from 'redux';
 import { RouterReducer, RouterReducerState } from './router';
 import { ModuleReducer, ModuleReducerState } from './module';
 import { CohortReducer } from './cohort';
+import { authReducer } from './auth';
 
-export type RootReducerState = RouterReducerState & ModuleReducerState & { cohort: any };
+export type RootReducerState = RouterReducerState & ModuleReducerState & { cohort: any, auth: {loggedInUser: any} };
 
 export const rootReducer: Reducer<RootReducerState> = combineReducers({
     ...RouterReducer,
     ...ModuleReducer,
     ...CohortReducer,
+    auth: authReducer
 });

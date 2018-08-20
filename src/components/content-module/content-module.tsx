515ebd2632e3261a43bf55ed35018dab1b90f6e2
Anya Craig <AnyaCraig@users.noreply.github.com>
2018-08-20 10:49:09 -0400

    Refactored component to use implicit return.

--- src/components/content-module/content-module.tsx
+++ src/components/content-module/content-module.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import { ContentModule } from '../../constants';
 
-export const ModuleComponent = ({module}: {module: ContentModule}) => {
-
-  return (
+export const ModuleComponent = ({module}: {module: ContentModule}) => (
+  
     <article className="center mw5 mw6-ns br3 hidden ba b--black-10 mv4 tc">
     <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">
       <span>{module.name}</span>
@@ -20,7 +19,7 @@ export const ModuleComponent = ({module}: {module: ContentModule}) => {
       <h3>Content</h3>
       <a href={module.content} target="_blank">{module.content}</a>
     </div>
-    
+
     {(module.challenges && module.challenges.length > 0) &&
       <div className="w-100 black">
         <h3>In Class Challenges</h3>
@@ -43,5 +42,4 @@ export const ModuleComponent = ({module}: {module: ContentModule}) => {
       </div>
     }
   </article>
-  );
-}; 
+);

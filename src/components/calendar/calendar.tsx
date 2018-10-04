9a9c96eab1cc0dd10d3effac2fc4a1f26ab3e878
Abdella Ali <della@Abdellas-MacBook-Pro.local>
2018-10-04 10:51:43 -0400

    disabled dates before start date

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-export const CalendarComponent = ({handleDayClick, selectedDates}) => {
+export const CalendarComponent = (props) => {
 
     const highlightStyle = `.DayPicker-Day--selected:not(.DayPicker-Day--disabled):not(.DayPicker-Day--outside) {
         background-color: orange;
@@ -13,7 +13,7 @@ export const CalendarComponent = ({handleDayClick, selectedDates}) => {
         <div className="calendar-root flex justify-center" style={{minHeight: '300px'}}>
             <div className="calendar flex ba bw3 b--light-gray">
                 <style>{highlightStyle}</style>
-                <DayPicker onDayClick={handleDayClick} selectedDays={selectedDates}/>
+                <DayPicker {...props} />
             </div>
         </div>
     );
